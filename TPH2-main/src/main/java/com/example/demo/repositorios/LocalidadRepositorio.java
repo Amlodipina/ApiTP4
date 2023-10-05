e86fed11750fae57d0dc4abe87dc4480e3aeb5b0
Amlodipina <leonardosecotaro@gmail.com>
2023-10-05 15:56:08 -0300

    2

--- TPH2-main/src/main/java/com/example/demo/repositorios/LocalidadRepositorio.java
+++ TPH2-main/src/main/java/com/example/demo/repositorios/LocalidadRepositorio.java
@@ -1,6 +1,9 @@
 package com.example.demo.repositorios;
 
 import com.example.demo.entidades.Localidad;
+import org.springframework.stereotype.Repository;
+
+@Repository
 
 public interface LocalidadRepositorio extends BaseRepositorio<Localidad, Long>{
 }
